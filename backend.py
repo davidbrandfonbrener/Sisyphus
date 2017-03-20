d8c4e2046151457c2269c533473baa15355950d7
Daniel <daniel@MacBook-Pro.local>
2017-03-20 17:38:24 -0400

    gamma initialization, using reg_loss instead of built in, fixed plotting W, added tasks to xor, removed autapse restriction

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 class Model(object):
-    def __init__(self, n_in, n_hidden, n_out, n_steps, tau, dale_ratio, rec_noise, batch_size):
+    def __init__(self, n_in, n_hidden, n_out, n_steps, tau, dt, dale_ratio, rec_noise, batch_size):
         # network size
         self.n_in = n_in
         self.n_hidden = n_hidden
@@ -15,7 +15,7 @@ class Model(object):
         self.batch_size = batch_size
 
         # neuro parameters
-        self.dt = 0.1
+        self.dt = dt
         self.tau = tau
         self.alpha = self.dt / self.tau
         self.dale_ratio = dale_ratio
@@ -23,26 +23,33 @@ class Model(object):
 
         # dale matrix
         dale_vec = np.ones(n_hidden)
-        dale_vec[int(dale_ratio * n_hidden):] = -1
-        self.dale_rec = np.diag(dale_vec)
-        dale_vec[int(dale_ratio * n_hidden):] = 0
-        self.dale_out = np.diag(dale_vec)
+        
+        if self.dale_ratio:
+            dale_vec[int(self.dale_ratio * n_hidden):] = -1
+            self.dale_rec = np.diag(dale_vec)
+            dale_vec[int(self.dale_ratio * n_hidden):] = 0
+            self.dale_out = np.diag(dale_vec)
+        else:
+            self.dale_rec = np.diag(dale_vec)
+            self.dale_out = np.diag(dale_vec)
 
         #connectivity
-        self.connect_mat = np.ones((n_hidden, n_hidden)) - np.diag(np.ones(n_hidden))
+        self.connect_mat = np.ones((n_hidden, n_hidden)) #- np.diag(np.ones(n_hidden))
 
         # tensorflow initializations
         self.x = tf.placeholder("float", [batch_size, n_steps, n_in])
         self.y = tf.placeholder("float", [batch_size, n_steps, n_out])
         self.output_mask = tf.placeholder("float", [batch_size, n_steps, n_out])
 
-        self.init_state = tf.random_normal([batch_size, n_hidden], mean=0.0, stddev=rec_noise)
-
         # trainable variables
         with tf.variable_scope("model"):
-            self.U = tf.get_variable('U', [n_hidden, n_in], initializer=tf.random_normal_initializer(stddev=.01))
-            self.W = tf.get_variable('W', [n_hidden, n_hidden], initializer=tf.random_normal_initializer(stddev=.01))#initializer=tf.constant_initializer(self.initial_W()))
-            self.Z = tf.get_variable('Z', [n_out, n_hidden], initializer=tf.random_normal_initializer(stddev=.01))
+            
+            self.init_state = tf.get_variable('init_state',[batch_size, n_hidden],initializer=tf.random_normal_initializer(mean=0.1, stddev=0.01))
+            
+            self.U = tf.get_variable('U', [n_hidden, n_in], initializer=tf.constant_initializer(0.1*np.random.uniform(-1,1,size=(self.n_hidden,self.n_in)))) ##uniform initialization as in pycog
+            self.W = tf.get_variable('W', [n_hidden, n_hidden], initializer=tf.constant_initializer(self.initial_W())) #gamma (dale) or normal (non-dale) initialization
+            self.Z = tf.get_variable('Z', [n_out, n_hidden], initializer=tf.constant_initializer(0.1*np.random.uniform(-1,1,size=(self.n_out,self.n_hidden)))) #uniform initialization as in pycog
+
             self.Dale_rec = tf.get_variable('Dale_rec', [n_hidden, n_hidden],
                                             initializer=tf.constant_initializer(self.dale_rec),
                                             trainable=False)
@@ -56,17 +63,26 @@ class Model(object):
             self.bout = tf.get_variable('bout', [n_out], initializer=tf.constant_initializer(0.0))
 
             self.predictions, self.states = self.compute_predictions()
-            self.loss = tf.losses.mean_squared_error(self.y, self.predictions, weights=self.output_mask)
+            self.loss = self.reg_loss() #tf.losses.mean_squared_error(self.y, self.predictions, weights=self.output_mask)
 
     # implement one step of the RNN
 
     def rnn_step(self, rnn_in, state):
-        new_state = state * (1-self.alpha) + self.alpha * (tf.matmul(tf.nn.relu(state),
-                                                               tf.matmul(tf.abs(self.W) * self.Connectivity, self.Dale_rec, name="in1"),
-                                                               transpose_b=True, name="1")
-                                                     + tf.matmul(rnn_in, tf.abs(self.U), transpose_b=True, name="2") + self.brec) + \
+        
+        if self.dale_ratio:
+            new_state = (1-self.alpha) * state + self.alpha * (tf.matmul(tf.nn.relu(state),
+                                                                tf.matmul(tf.abs(self.W) * self.Connectivity, self.Dale_rec, name="in1"),transpose_b=True, name="1") + \
+                                                                tf.matmul(rnn_in, tf.abs(self.U), transpose_b=True, name="2") + self.brec) + \
+                                                                tf.random_normal(state.get_shape(), mean=0.0, stddev=self.rec_noise)
+            new_output = tf.matmul(tf.nn.relu(new_state), tf.matmul(tf.abs(self.Z), self.Dale_out, name="in2"), transpose_b=True, name="3") + self.bout
+
+        else:
+            new_state = ((1-self.alpha) * state) + (self.alpha * (tf.matmul(tf.nn.relu(state),self.W,transpose_b=True, name="1")                                 
+                                                     + tf.matmul(rnn_in, tf.abs(self.U), transpose_b=True, name="2") + self.brec)) + \
                                                         tf.random_normal(state.get_shape(), mean=0.0, stddev=self.rec_noise)
-        new_output = tf.matmul(tf.nn.relu(new_state), tf.matmul(tf.abs(self.Z), self.Dale_out, name="in2"), transpose_b=True, name="3") + self.bout
+            new_output = tf.matmul(tf.nn.relu(new_state),self.Z, transpose_b=True, name="3") + self.bout
+
+            
         return new_output, new_state
 
     # apply the step to a full input vector
@@ -86,18 +102,37 @@ class Model(object):
 
     # regularized loss function
     def reg_loss(self):
-        return tf.reduce_mean(tf.square(self.predictions - self.y))
+        return tf.reduce_mean(tf.square(self.predictions - self.y))# + tf.reduce_mean(tf.square(self.states))
 
     #fix spectral radius of recurrent matrix
     def initial_W(self):
-        W = np.matmul(abs(np.random.normal(scale=.01, size=(self.n_hidden, self.n_hidden))), self.dale_rec)
-        rho = max(abs(np.linalg.eigvals(W)))
-        return (1.1/rho) * W
+        
+        #added gamma distributed initial weights as in pycog
+        if self.dale_ratio:
+            self.W_dist0 = 'gamma'
+        else:
+            self.W_dist0 = 'normal'
+        
+        if self.W_dist0 == 'normal':
+            w0 = np.random.normal(scale=1, size=(self.n_hidden, self.n_hidden))
+        elif self.W_dist0 == 'gamma':
+            k = 2
+            theta = .1/k
+            w0 = np.random.gamma(k, theta, size=(self.n_hidden,self.n_hidden)) 
+                
+        
+        if self.dale_ratio:
+            W = np.matmul(abs(w0), self.dale_rec)
+        else:
+            W = w0
+            
+        rho = max(abs(np.linalg.eigvals(W)))#+np.diag(np.ones(self.n_hidden)*(1-self.alpha))))) #add diagnal matrix 1-alpha to account for persistance tau
+        return (1.1/rho) * W  # - .9*np.diag(np.ones(self.n_hidden)*(1-self.alpha)) #correct for tau
 
 
 # train the model using Adam
-def train(sess, model, generator, learning_rate, training_iters, batch_size, display_step):
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.loss)
+def train(sess, model, generator, learning_rate, training_iters, batch_size, display_step,dale):
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.loss,var_list=[model.W,model.U,model.Z,model.brec,model.bout,model.init_state])
     sess.run(tf.global_variables_initializer())
     step = 1
     # Keep training until reach max iterations
@@ -111,7 +146,11 @@ def train(sess, model, generator, learning_rate, training_iters, batch_size, dis
                   "{:.6f}".format(loss))
         step += 1
     print("Optimization Finished!")
-    plt.imshow(np.matmul(abs(model.W.eval(session=sess)), model.dale_rec), interpolation="none")
+    if dale:
+        plt.pcolor(np.matmul(abs(model.W.eval(session=sess)), model.dale_rec))
+    else:
+        plt.pcolor(model.W.eval(session=sess))
+    plt.colorbar()
     plt.show()
     #plt.imshow(np.matmul(abs(model.Z.eval(session=sess)), model.dale_out), interpolation="none")
     #plt.show()
@@ -119,11 +158,11 @@ def train(sess, model, generator, learning_rate, training_iters, batch_size, dis
 
 # use a trained model to get test outputs
 def test(sess, model, input):
-    preds = sess.run(model.predictions, feed_dict={model.x: input})
-    return preds
+    preds,states = sess.run([model.predictions, model.states], feed_dict={model.x: input})
+    return preds,states
 
 #visualize network output on a trial, compared to desired output
 def visualize_trial(sess, model, input, desired_output):
-    preds = test(sess, model, input)
+    preds,states = test(sess, model, input)
     plt.plot()
     return
